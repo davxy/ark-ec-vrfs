f6cb3743aa521f8620db728bb50bc10bccf747ce
Davide Galassi <davxy@datawok.net>
2024-04-01 20:43:29 +0200

    Some cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,51 @@ use ark_ff::PrimeField;
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Valid};
 use ark_std::{vec, vec::Vec};
 
+// see RFC9381 Section 7.10
+const SUITE_ID: u8 = 0x04;
+// In typical case us qLen / 2
+const CLEN: usize = 16;
+const DOM_SEP_START: u8 = 0x02;
+const DOM_SEP_END: u8 = 0x00;
+
+// Hasher
+#[inline(always)]
+fn sha512(input: &[u8]) -> [u8; 64] {
+    use sha2::{Digest, Sha512};
+    let mut hasher = Sha512::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+    let mut h = [0u8; 64];
+    h.copy_from_slice(&result);
+    h
+}
+
+/// ECVRF nonce generation according to Section 5.1.6 of [RFC8032](https://tools.ietf.org/html/rfc8032).
+#[inline(always)]
+pub fn nonce_gen<P: AffineRepr>(sk: &Secret<P>, pt: Input<P>) -> <P as AffineRepr>::ScalarField {
+    let mut buf = Vec::new();
+    sk.scalar.serialize_compressed(&mut buf).unwrap();
+    let sk_hash = &sha512(&buf)[32..];
+    buf.clear();
+    pt.0.serialize_compressed(&mut buf).unwrap();
+    let v = [sk_hash, &buf[..]].concat();
+    let h = sha512(&v);
+    <P as AffineRepr>::ScalarField::from_le_bytes_mod_order(&h)
+}
+
+/// `ECVRF_challenge_generation` -- Hashes several points on the curve
+#[inline(always)]
+pub fn challenge_gen<P: AffineRepr>(points: &[&P], ad: &[u8]) -> <P as AffineRepr>::ScalarField {
+    let mut buf = vec![SUITE_ID, DOM_SEP_START];
+    points.into_iter().for_each(|p| {
+        p.serialize_compressed(&mut buf).unwrap();
+    });
+    buf.extend_from_slice(ad);
+    buf.push(DOM_SEP_END);
+    let hash = &sha512(&buf)[..CLEN];
+    <P as AffineRepr>::ScalarField::from_be_bytes_mod_order(hash)
+}
+
 /// Secret key
 // TODO: zeroize
 #[derive(Debug, PartialEq)]
@@ -53,40 +98,9 @@ impl<P: AffineRepr> Valid for Secret<P> {
     }
 }
 
-/// Public key
-#[derive(Debug, Copy, Clone, PartialEq, CanonicalSerialize, CanonicalDeserialize)]
-pub struct Public<P: AffineRepr>(pub P);
-
-impl<P: AffineRepr> Public<P> {
-    pub fn verify(
-        &self,
-        input: Input<P>,
-        ad: impl AsRef<[u8]>,
-        signature: &Signature<P>,
-    ) -> Result<(), ()> {
-        let Signature {
-            output,
-            proof: (c, s),
-        } = signature;
-
-        let s_b = P::generator() * s;
-        let c_y = self.0 * c;
-        let u = (s_b - c_y).into_affine();
-
-        let s_h = input.0 * s;
-        let c_o = signature.output.0 * c;
-        let v = (s_h - c_o).into_affine();
-
-        let c_exp = challenge_gen(&[&self.0, &input.0, &output.0, &u, &v], ad.as_ref());
-
-        (&c_exp == c).then(|| ()).ok_or(())
-    }
-}
-
 impl<P: AffineRepr> Secret<P> {
     pub fn from_scalar(scalar: <P as AffineRepr>::ScalarField) -> Self {
-        let g = P::generator();
-        let public = g * scalar;
+        let public = P::generator() * scalar;
         let public = Public(public.into_affine());
         Self { scalar, public }
     }
@@ -130,41 +144,34 @@ impl<P: AffineRepr> Secret<P> {
     }
 }
 
-/// ECVRF nonce generation according to Section 5.1.6 of [RFC8032](https://tools.ietf.org/html/rfc8032).
-pub fn nonce_gen<P: AffineRepr>(sk: &Secret<P>, pt: Input<P>) -> <P as AffineRepr>::ScalarField {
-    let mut sk_enc = Vec::new();
-    sk.scalar.serialize_compressed(&mut sk_enc).unwrap();
-    let key_hash: [u8; 32] = sha512(&sk_enc)[32..].try_into().unwrap();
-    let mut pt_enc = Vec::new();
-    pt.0.serialize_compressed(&mut pt_enc).unwrap();
-    let v = [&key_hash[..], &pt_enc[..]].concat();
-    let h = sha512(&v);
-    <P as AffineRepr>::ScalarField::from_be_bytes_mod_order(&h)
-}
+/// Public key
+#[derive(Debug, Copy, Clone, PartialEq, CanonicalSerialize, CanonicalDeserialize)]
+pub struct Public<P: AffineRepr>(pub P);
 
-// see RFC9381 Section 7.10
-const SUITE_ID: u8 = 0x04;
-// In typical case us qLen / 2
-const CLEN: usize = 16;
-const DOM_SEP_START: u8 = 0x02;
-const DOM_SEP_END: u8 = 0x00;
+impl<P: AffineRepr> Public<P> {
+    pub fn verify(
+        &self,
+        input: Input<P>,
+        ad: impl AsRef<[u8]>,
+        signature: &Signature<P>,
+    ) -> Result<(), ()> {
+        let Signature {
+            output,
+            proof: (c, s),
+        } = signature;
 
-fn encode_point<P: AffineRepr>(p: &P) -> Vec<u8> {
-    let mut buf = Vec::new();
-    p.serialize_compressed(&mut buf).unwrap();
-    buf
-}
+        let s_b = P::generator() * s;
+        let c_y = self.0 * c;
+        let u = (s_b - c_y).into_affine();
 
-/// `ECVRF_challenge_generation` -- Hashes several points on the curve
-pub fn challenge_gen<P: AffineRepr>(points: &[&P], ad: &[u8]) -> <P as AffineRepr>::ScalarField {
-    let mut v = vec![SUITE_ID, DOM_SEP_START];
-    points
-        .into_iter()
-        .for_each(|p| v.extend_from_slice(encode_point(*p).as_slice()));
-    v.extend_from_slice(ad);
-    v.push(DOM_SEP_END);
-    let h = &sha512(&v)[..CLEN];
-    <P as AffineRepr>::ScalarField::from_be_bytes_mod_order(h)
+        let s_h = input.0 * s;
+        let c_o = signature.output.0 * c;
+        let v = (s_h - c_o).into_affine();
+
+        let c_exp = challenge_gen(&[&self.0, &input.0, &output.0, &u, &v], ad.as_ref());
+
+        (&c_exp == c).then(|| ()).ok_or(())
+    }
 }
 
 // TODO macro for point newtypes
@@ -202,16 +209,6 @@ pub struct Signature<P: AffineRepr> {
         <P as AffineRepr>::ScalarField,
         <P as AffineRepr>::ScalarField,
     ),
-}
-
-fn sha512(input: &[u8]) -> [u8; 64] {
-    use sha2::{Digest, Sha512};
-    let mut hasher = Sha512::new();
-    hasher.update(input);
-    let result = hasher.finalize();
-    let mut h = [0u8; 64];
-    h.copy_from_slice(&result);
-    h
 }
 
 #[cfg(test)]
